b4de087dafbcedbf4a58301052072e56a3850926
hattiee2003 <50850607+hattiee2003@users.noreply.github.com>
2020-05-27 11:40:07 +1200

    facts version 1

---
+++ functions/random-fact.js
@@ -0,0 +1,16 @@
+const facts = [
+    'Fact one.',
+    'Fact two.',
+    'Fact three.',
+    'Fact four.'
+]
+
+exports.handler = async (event, context) => {
+    const index = Math.floor(Math.random() * facts.length)
+    const facts = facts[index]
+
+    return {
+        statusCode: 200,
+        body: fact
+    }
+}
