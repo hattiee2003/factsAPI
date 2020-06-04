83f1c1dd7f6e404ff2f8604a02ff456d9438fed7
hattiee2003 <50850607+hattiee2003@users.noreply.github.com>
2020-06-05 11:06:44 +1200

    updATE

--- functions/random-fact.js
+++ functions/random-fact.js
@@ -1,15 +1,15 @@
-const facts = [
-    'I like turtles🐢',
-	'I lived on a farm once🌱',
-	'I lived in Singapore',
-	'I can swim🏊🏼‍♀️',
-	'I love dumplings',
-	'Why Dont We is my favourite band🎵'
+let facts = [
+    {fact: 'I have a dog 🐕', real: true},
+    {fact: 'I lived in Malaysia 🏠', real: false},
+    {fact: 'I lived on a farm once 🌱', real: true},
+    {fact: 'I love buttered toast 🍞', real: false},
+    {fact: 'Shawn Mendes is my fav singer 🎵', real: true},
+    {fact: 'Monkeys are my favourite animal 🐒', real: false}
 ]
 
 exports.handler = async (event, context) => {
-    const index = Math.floor(Math.random() * facts.length)
-    const fact = facts[index]
+    let index = Math.floor(Math.random() * facts.length)
+    outputFact = facts[index].fact
 
     return {
         statusCode: 200,
